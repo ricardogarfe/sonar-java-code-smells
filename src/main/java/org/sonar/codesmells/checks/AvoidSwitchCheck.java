4c13087523c0965b3ae006ae35f60878a54bd947
Ricardo García Fernández <ricardogarfe@gmail.com>
2015-12-14 12:40:50 +0100

    Workaround to show info on rule description

--- src/main/java/org/sonar/codesmells/checks/AvoidSwitchCheck.java
+++ src/main/java/org/sonar/codesmells/checks/AvoidSwitchCheck.java
@@ -12,7 +12,33 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 
 @Rule(key = "AvoidSwitch", name = "\"switch\" statements are evil", tags = {
-        "error", "apocalypse"}, description = "AvoidSwitch", priority = Priority.BLOCKER)
+        "error", "apocalypse"}, description = "<p>You have a conditional that chooses different behavior depending on the type of an object.</p>\n" +
+        "\n" +
+        "<h2>Noncompliant Code Example</h2>\n" +
+        "\n" +
+        "<pre>\n" +
+        "double getSpeed() {\n" +
+        "  switch (_type) {\n" +
+        "    case EUROPEAN:\n" +
+        "      return getBaseSpeed();\n" +
+        "    case AFRICAN:\n" +
+        "      return getBaseSpeed() - getLoadFactor() * _numberOfCoconuts;\n" +
+        "    case NORWEGIAN_BLUE:\n" +
+        "      return (_isNailed) ? 0 : getBaseSpeed(_voltage);\n" +
+        "  }\n" +
+        "  throw new RuntimeException (\"Should be unreachable\");\n" +
+        "}\n" +
+        "</pre>\n" +
+        "\n" +
+        "<h2>Compliant Solution</h2>\n" +
+        "\n" +
+        "<p>Move each leg of the conditional to an overriding method in a subclass. Make the original method abstract.</p>\n" +
+        "\n" +
+        "<ul>\n" +
+        "    <li><a href=\"http://www.refactoring.com/catalog/replaceConditionalWithPolymorphism.html\">Replace Conditional with\n" +
+        "        Polymorphism</a> - Fowler Refactoring Catalog\n" +
+        "    </li>\n" +
+        "</ul>", priority = Priority.BLOCKER)
 @ActivatedByDefault
 @SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.API_ABUSE)
 @SqaleConstantRemediation("2min")
